a0c76a74b3db148d53db0acc7b98db7989201db7
JOSE ERIVAN <j.erivan.pereira@gmail.com>
2024-09-30 20:37:13 -0300

    Projeto contador

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,8 @@
+public class Contador {
+
+    public static void main(String[] args) {   
+        for (int parametroUm = 10; parametroUm <= 20; parametroUm++) {
+        System.out.println(" Contando parametros " + parametroUm);
+              }
+              }
+            }
