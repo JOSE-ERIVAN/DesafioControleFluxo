1018bbc7bd7e649f432eab076cd8230d2075c541
JOSE ERIVAN <j.erivan.pereira@gmail.com>
2026-01-09 00:16:17 -0300

    altera

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,7 +1,7 @@
 public class Contador {
 
     public static void main(String[] args) {   
-        for (int parametroUm = 10; parametroUm <= 20; parametroUm++) {
+        for (int parametroUm = 100; parametroUm <= 20; parametroUm++) {
         System.out.println(" Contando parametros " + parametroUm);
               }
               }
